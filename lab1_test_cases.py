f7e32d0b60f78b42c43be1359c7eba3c53b3833a
dramir63 <dramir63@calpoly.edu>
2019-04-09 17:45:22 -0700

    -complete w/ comments

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -5,72 +5,99 @@ from lab1 import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
+        #Test if list is None
         """add description here"""
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
     def test_max_list_iter_last(self):
+        # Test if last number is max
         tlist = [1, 2, 3]
         self.assertEqual(max_list_iter(tlist), 3)
 
     def test_max_list_iter_first(self):
+        # Test if first number is max
         tlist = [3, 2, 1]
         self.assertEqual(max_list_iter(tlist), 3)
 
     def test_max_list_iter_all(self):
+        # Test max if all numbers are the same
         tlist = [0, 0, 0]
         self.assertEqual(max_list_iter(tlist), 0)
 
     def test_max_list_iter_negative(self):
+        # Test if numbers in list are negative
         tlist = [-2, -1, -3]
         self.assertEqual(max_list_iter(tlist), -1)
 
     def test_max_list_iter_empty(self):
+        # Test if list is empty
         tlist = []
         self.assertEqual(max_list_iter(tlist), None)
 
 
     def test_reverse_rec_none(self):
+        # Test if list is None
         rlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             reverse_rec(rlist)
 
+
     def test_reverse_rec(self):
+        # Test if function works as intended
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
-    def test_reverse_rec_01(self):
+
+
+    def test_reverse_rec_all(self):
+        # Test if all numbers in list are same
         self.assertEqual(reverse_rec([0, 0, 0]),[0, 0, 0])
+
+
     def test_reverse_rec_02(self):
+        # Test if all but two numbers are the same
         self.assertEqual(reverse_rec([1, 0, 0]),[0, 0, 1])
-    def test_reverse_rec_03(self):
+
+
+    def test_reverse_rec_negative(self):
+        # Test if list has a negative number
         self.assertEqual(reverse_rec([-1, 0, 1]),[1, 0, -1])
 
 
     def test_bin_search(self):
+        # Test if functions works as intended
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
 
+
     def test_bin_search_first(self):
+        # Test if target is first number in list
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val) - 1
         self.assertEqual(bin_search(0, 0, len(list_val)-1, list_val), 0 )
 
+
     def test_bin_search_last(self):
+        # Test if target is last number in list
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val) - 1
         self.assertEqual(bin_search(10, 0, len(list_val)-1, list_val), 8)
 
+
     def test_bin_search_not_found(self):
+        # Test if target is not found
         list_val = [0, 1, 2, 3, 4, 7, 8, 9, 10]
         low = 0
         high = len(list_val) - 1
         self.assertEqual(bin_search(15, 0, len(list_val) - 1, list_val), None)
 
+
     def test_bin_search_none(self):
+        # Test if list is None
         blist = None
         with self.assertRaises(ValueError):  # used to check for exception
             bin_search(5, 0, 5, blist)
--- location_tests.py
+++ location_tests.py
@@ -5,15 +5,23 @@ class TestLab1(unittest.TestCase):
 
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
+        loc2 = Location("LA", 34.0, 118.24)
+        loc5 = Location("San Francisco", 37.78, 122.4)
         self.assertEqual(repr(loc),"Location(SLO, 35.3, -120.7)")
+        self.assertEqual(repr(loc2), "Location(LA, 34.0, 118.24)")
+        self.assertIsNot(repr(loc), repr(loc5))
         #print("done")
 
     def test_eq(self):
         loc = Location("SLO", 35.3, -120.7)
         loc2 = Location("SLO", 35.3, -120.7)
         loc3 = Location("LA", 34.0, 118.24)
+        loc4 = Location("LA", 34.0, 118.24)
+        loc5 = Location("San Francisco", 37.78, 122.4)
         self.assertEqual(loc, loc2)
         self.assertIsNot(loc, loc3)
+        self.assertEqual(loc3, loc4)
+        self.assertIsNot(loc5, loc4)
         #print("done")
     
     # Add more tests!
